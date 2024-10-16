b1ec4403de7acb3b747104b8cf029adc2511f591
levitate777 <and.dickiy@gmail.com>
2024-10-16 20:06:21 +0300

    feat(api): added token substitution in requests and created a function for sending data for editing a user

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -3,3 +3,15 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: import.meta.env.VITE_APP_API_URL
 });
+
+api.interceptors.request.use(
+	(config) => {
+		const { headers } = config;
+		const token = localStorage.getItem('token');
+		if (token !== null) {
+			headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => Promise.reject(error),
+);
--- src/redux/api/userApi.ts
+++ src/redux/api/userApi.ts
@@ -1,8 +1,25 @@
 import { AxiosResponse } from 'axios';
 
-import { IUser } from '../../types/userTypes';
+import { IUserDataUpdate, IUserStateData } from '../../types/userTypes';
 import { api } from '.';
 
+export const updateUser = (payload: IUserDataUpdate): Promise<AxiosResponse> => { 
+	const { id, login, avatar } = payload;
+	const formData = new FormData();
+	formData.append('id', id);
+	if (login) formData.append('login', login);
+	if (avatar) formData.append('avatar', avatar);
+	for (const [key, value] of formData) {
+    console.log(`${key}: ${value}\n`);
+  }
+
+  return api.patch<IUserStateData>(
+		`user`,
+		formData,
+		{ headers: { 'Content-Type': 'multipart/form-data' } },
+	);
+};
+
 export const whoIsThisRequest = (): Promise<AxiosResponse> => {
-	return api.get<IUser>('user/whoIsThis');
+	return api.get<IUserStateData>('user/whoIsThis');
 };
